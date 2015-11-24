3175e3d9ac7a0bb4ab64a37ee09188ef4614d67c
Kris Stewart <kris_stewart@mastercard.com>
2015-11-24 21:27:13 +0000

    kds chapter 13 exercises

---
+++ chapter13/kds/ex1.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+print "Enter a directory:\n";
+chomp( my $dir = <STDIN> );
+
+chdir $dir or die "...can't open $dir" if( $dir =~ /\S/ );
+chdir or die "...can't open home dir" if( $dir =~ /\s/ );
+
+print $_ . "\n" foreach( sort glob "*" );
+
