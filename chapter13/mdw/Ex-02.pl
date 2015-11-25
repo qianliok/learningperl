7f42a95e24278cd7236dbd9fc0e59a1b57dcb22e
Mark Wignall <mark.wignall@datacash.com>
2015-11-25 09:44:56 +0000

    Exercise for chapter 13

---
+++ chapter13/mdw/Ex-02.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use feature "say";
+
+say "What director do you want to change to and print the contents of?";
+
+chomp (my $dirname = <>);
+
+if ($dirname ne ""){
+	chdir $dirname or die "Failed to change directory to $dirname";
+} else {
+	chdir or die "Failed to change to home directory";
+}
+my @contents = sort glob '.* *';
+
+foreach my $file (@contents) {
+	say $file;
+}
